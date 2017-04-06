5d45630c370987d47ee3c97e5cf65c8590111305
Elliot Harris <elliotharris@live.co.uk>
2017-04-06 16:02:51 +0100

    Added more comments to the main file for bf-cli

--- bf-cli/src/main.rs
+++ bf-cli/src/main.rs
@@ -1,14 +1,18 @@
+// Load the main BF library
 extern crate bf_lib;
 
+// Import the tools we need from BF library
 use bf_lib::traits::*;
 use bf_lib::bf_lexer::BFLexer;
 use bf_lib::bf_vm::BFVM;
 use bf_lib::bf_vm::VMSettings;
-use std::process;
 
+// Import various STD library components
+use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
+// Command line arguments are parsed using a simpel state machine
 #[derive(Debug)]
 enum ArgumentMode {
     Skip,
@@ -17,7 +21,11 @@ enum ArgumentMode {
     File,
 }
 
+// Reads a file and puts the contents into the out_str String.
 fn read_file(in_str : String, mut out_str : &mut String ) {
+    // TODO: Return an code here if needed and then print out
+    //  a usable error message.
+    // Currently panics on failure (expect)!
     let mut file = File::open(in_str).expect("Unable to open file.");
     file.read_to_string(&mut out_str).expect("Unable to read file.");
 }
@@ -42,9 +50,14 @@ fn main() {
     /* ---------------------------------------------------.
     |     Load Arguments                                  |
     '---------------------------------------------------- */
+
+    // Argument Setup - Skip first argument as it's the binary location.
     let mut mode = ArgumentMode::Skip;
     let mut input = String::new();
     let mut settings = VMSettings::new();
+
+    // Loop through each argument and set various settings as per the state
+    // Aka if in Str mode the next argument will be considered the input.
     for argument in std::env::args() {
         match mode {
             ArgumentMode::Skip => mode = ArgumentMode::Start,
@@ -75,9 +88,17 @@ fn main() {
     /* ---------------------------------------------------.
     |    Interpret and Run Input                          |
     '---------------------------------------------------- */
+
+    // Parse string input into Vector of BFTokens
+    // This step also matches brackets up to each other
     let tokens = BFLexer::parse(String::from(input));
+
+    // Create a new VM instance
     let mut bfvm = BFVM::new(settings);
 
+    // If parsing was successful run the script, 
+    //  otherwise Error
+    // Currently parsing can't fail.
     let result = match tokens {
         LexResult::Success(t) => bfvm.run(t),
         _ =>  {
@@ -85,11 +106,18 @@ fn main() {
             VMResult::Error { message : String::from("Uh oh") }
         }
     };
+
+    // In case there was no new line printed
+    //  print one now before exit so prompt is on
+    //  new line if running from command line.
     println!();
 
     /* ---------------------------------------------------.
     |    Output Result                                    |
     '---------------------------------------------------- */
+
+    // Setup exit code - 0 = Success
+    // Currently all error's except panics are 1
     let return_code = if result == VMResult::Success { 0 } else { 1 };
     process::exit(return_code);
 }
